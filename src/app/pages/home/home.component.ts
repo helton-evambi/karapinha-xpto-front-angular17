72ab78f4accb8bf1434e290c28ec92aa29adc4ad
Helton Evambi <heltonevambi@gmail.com>
2024-05-16 17:23:22 +0100

    create home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { ButtonComponent } from '../../components/button/button.component';
+import { CardCategoryComponent } from '../../components/card-category/card-category.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ButtonComponent, CardCategoryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
