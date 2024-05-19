c2d51218ecf19035f494421ee30fa8dd35daef95
Helton Evambi <heltonevambi@gmail.com>
2024-05-19 16:15:55 +0100

    create testimorials section

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 
+type CarouselType = 'testimonials' | 'image';
 @Component({
   selector: 'app-carousel',
   standalone: true,
@@ -9,14 +10,22 @@ import { Component, Input } from '@angular/core';
 })
 export class CarouselComponent {
   @Input() images: string[] = [];
+  @Input() type: CarouselType = 'image';
+  @Input() testimonials: { text: string; author: string }[] = [];
   currentSlide = 1;
 
   nextSlide() {
-    this.currentSlide = (this.currentSlide + 1) % this.images.length;
+    if (this.type === 'image')
+      this.currentSlide = (this.currentSlide + 1) % this.images.length;
+    else this.currentSlide = (this.currentSlide + 1) % this.testimonials.length;
   }
 
   prevSlide() {
-    this.currentSlide =
-      (this.currentSlide - 1 + this.images.length) % this.images.length;
+    if (this.type === 'image')
+      this.currentSlide =
+        (this.currentSlide - 1 + this.images.length) % this.images.length;
+    else
+      this.currentSlide =
+        (this.currentSlide - 1 + this.images.length) % this.testimonials.length;
   }
 }
