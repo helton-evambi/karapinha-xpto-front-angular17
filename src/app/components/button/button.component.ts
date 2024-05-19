328d7f1f771de7dd45aa233e71515049fb1d6a7c
Helton Evambi <heltonevambi@gmail.com>
2024-05-19 23:04:07 +0100

    add size prop to button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 type BtnVariants = 'primary' | 'secundary';
 type BtnColors = 'normal' | 'pink';
+type BtnSize = 'normal' | 'full';
 
 @Component({
   selector: 'app-button',
@@ -15,6 +16,7 @@ export class ButtonComponent {
   @Input() disabled: boolean = false;
   @Input() variant: BtnVariants = 'primary';
   @Input() color: BtnColors = 'normal';
+  @Input() size: BtnSize = 'normal';
 
   @Output('submit') onSubmit = new EventEmitter();
 
