8777609f21b17a1179d44989595bf576a92dade2
Helton Evambi <heltonevambi@gmail.com>
2024-05-27 15:51:04 +0100

    create booking page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ServicosComponent } from './pages/servicos/servicos.component';
+import { BookingComponent } from './pages/booking/booking.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'servicos',
     component: ServicosComponent,
+  },
+  {
+    path: 'booking',
+    component: BookingComponent,
   },
 ];
