bc9cfbfaa8aec9e93c445a4a20cef42b927ab97e
Helton Evambi <heltonevambi@gmail.com>
2024-05-27 13:27:00 +0100

    add footer to services page

--- src/app/pages/servicos/servicos.component.ts
+++ src/app/pages/servicos/servicos.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { ServicesContentComponent } from '../../components/services-content/services-content.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-servicos',
   standalone: true,
-  imports: [HeaderComponent, ServicesContentComponent],
+  imports: [HeaderComponent, ServicesContentComponent, FooterComponent],
   templateUrl: './servicos.component.html',
   styleUrl: './servicos.component.scss',
 })
