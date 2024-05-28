8da246c8e72963ea634feaf10978545bf93e06ef
Helton Evambi <heltonevambi@gmail.com>
2024-05-28 09:37:55 +0100

    change imports

--- src/app/pages/booking/booking.component.ts
+++ src/app/pages/booking/booking.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { BookingSummaryComponent } from '../../components/booking-summary/booking-summary.component';
 
 @Component({
   selector: 'app-booking',
   standalone: true,
-  imports: [],
+  imports: [BookingSummaryComponent],
   templateUrl: './booking.component.html',
   styleUrl: './booking.component.scss',
 })
 export class BookingComponent {
   @Input() stepTitle: string = '';
+  @Input() stepNumber: number = 1;
 }
