047457195783daee2eb289bb7fb616ae80c19c35
Helton Evambi <heltonevambi@gmail.com>
2024-05-28 19:12:42 +0100

    import icons

--- src/app/pages/booking/booking.component.ts
+++ src/app/pages/booking/booking.component.ts
@@ -1,10 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { BookingSummaryComponent } from '../../components/booking-summary/booking-summary.component';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { ionArrowBackOutline, ionCloseOutline } from '@ng-icons/ionicons';
 
 @Component({
   selector: 'app-booking',
   standalone: true,
-  imports: [BookingSummaryComponent],
+  imports: [BookingSummaryComponent, NgIconComponent],
+  viewProviders: [
+    provideIcons({
+      ionArrowBackOutline,
+      ionCloseOutline,
+    }),
+  ],
   templateUrl: './booking.component.html',
   styleUrl: './booking.component.scss',
 })
