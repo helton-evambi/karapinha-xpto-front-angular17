c05f13a24c39b819ec95a7f5285a8a90cdda23fc
Helton Evambi <heltonevambi@gmail.com>
2024-05-29 20:42:51 +0100

    create professional card in modal

--- src/app/pages/choose-professional/choose-professional.component.ts
+++ src/app/pages/choose-professional/choose-professional.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BookingComponent } from '../booking/booking.component';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { ionPersonAdd, ionChevronDown } from '@ng-icons/ionicons';
+import { ModalComponent } from '../../components/modal/modal.component';
 
 @Component({
   selector: 'app-choose-professional',
   standalone: true,
-  imports: [BookingComponent, NgIconComponent],
+  imports: [BookingComponent, NgIconComponent, ModalComponent],
   viewProviders: [
     provideIcons({
       ionPersonAdd,
@@ -16,4 +17,12 @@ import { ionPersonAdd, ionChevronDown } from '@ng-icons/ionicons';
   templateUrl: './choose-professional.component.html',
   styleUrl: './choose-professional.component.scss',
 })
-export class ChooseProfessionalComponent {}
+export class ChooseProfessionalComponent {
+  modalVisibility: boolean = false;
+  openModal() {
+    this.modalVisibility = true;
+  }
+  closeModal() {
+    this.modalVisibility = false;
+  }
+}
