4f2e3bfd80da19b8ee8893d1cc47df950f5fa6fc
Helton Evambi <heltonevambi@gmail.com>
2024-06-05 11:38:13 +0100

    subscribe comfirm-booking on observable

--- src/app/pages/confirm-booking/confirm-booking.component.ts
+++ src/app/pages/confirm-booking/confirm-booking.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { BookingComponent } from '../booking/booking.component';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { BookingService } from '../../services/booking.service';
+import { Booking } from '../../models/booking.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-confirm-booking',
   standalone: true,
-  imports: [BookingComponent],
+  imports: [BookingComponent, NgIf, AsyncPipe],
   templateUrl: './confirm-booking.component.html',
   styleUrl: './confirm-booking.component.scss',
 })
-export class ConfirmBookingComponent {}
+export class ConfirmBookingComponent implements OnInit {
+  private bookingService = inject(BookingService);
+  getBooking$!: Observable<Booking>;
+
+  ngOnInit(): void {
+    this.getBooking$ = this.bookingService.getBookingData();
+  }
+}
