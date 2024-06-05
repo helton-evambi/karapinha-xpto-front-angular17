0d575d9ffa1ec9b25b633be155616d6bd8154577
Helton Evambi <heltonevambi@gmail.com>
2024-06-05 11:40:00 +0100

    change booking model structre

--- src/app/models/booking-service.model.ts
+++ src/app/models/booking-service.model.ts
@@ -1,10 +1,9 @@
 import { Professional } from './professional.model';
 import { Service } from './service.model';
-import { Time } from './time.model';
 
 export interface BookingServices {
   Service: Service;
   Professional?: Professional;
-  Date?: Date;
+  Date?: string;
   TimeId?: number;
 }
