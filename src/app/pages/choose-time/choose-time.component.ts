f8d2ac9234cb6da8d8f2568cbda50125e762af7c
Helton Evambi <heltonevambi@gmail.com>
2024-06-05 11:42:29 +0100

    subscribe choose-time on observable

--- src/app/pages/choose-time/choose-time.component.ts
+++ src/app/pages/choose-time/choose-time.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit, inject } from '@angular/core';
 import { BookingComponent } from '../booking/booking.component';
 import { ionTimeOutline, ionChevronDown } from '@ng-icons/ionicons';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { ModalComponent } from '../../components/modal/modal.component';
+import { BookingService } from '../../services/booking.service';
+import { Observable } from 'rxjs';
+import { Booking } from '../../models/booking.model';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-choose-time',
   standalone: true,
-  imports: [BookingComponent, NgIconComponent, ModalComponent],
+  imports: [
+    BookingComponent,
+    NgIconComponent,
+    ModalComponent,
+    AsyncPipe,
+    NgIf,
+    FormsModule,
+  ],
   viewProviders: [
     provideIcons({
       ionTimeOutline,
@@ -17,12 +29,30 @@ import { ModalComponent } from '../../components/modal/modal.component';
   templateUrl: './choose-time.component.html',
   styleUrl: './choose-time.component.scss',
 })
-export class ChooseTimeComponent {
+export class ChooseTimeComponent implements OnInit {
+  private bookingService = inject(BookingService);
+  getBooking$!: Observable<Booking>;
   modalVisibility: boolean = false;
-  openModal() {
+
+  ngOnInit(): void {
+    this.getBooking$ = this.bookingService.getBookingData();
+  }
+
+  serviceId: number = 0;
+
+  openModal(serviceId: number) {
     this.modalVisibility = true;
+    this.serviceId = serviceId;
   }
   closeModal() {
     this.modalVisibility = false;
+  }
+
+  addDate(serviceId: number, date: string) {
+    this.bookingService.setDate(serviceId, date);
+  }
+  addTime(timeId: number) {
+    this.bookingService.setTime(this.serviceId, timeId);
+    this.closeModal();
   }
 }
