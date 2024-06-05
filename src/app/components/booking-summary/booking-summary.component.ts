fca3ae747aaf55a7b1f0645733a5defd0f943004
Helton Evambi <heltonevambi@gmail.com>
2024-06-05 11:38:57 +0100

    subscribe booking-summary on observable

--- src/app/components/booking-summary/booking-summary.component.ts
+++ src/app/components/booking-summary/booking-summary.component.ts
@@ -1,16 +1,26 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Booking } from '../../models/booking.model';
+import { BookingService } from '../../services/booking.service';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-booking-summary',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, NgIf],
   templateUrl: './booking-summary.component.html',
   styleUrl: './booking-summary.component.scss',
 })
-export class BookingSummaryComponent {
+export class BookingSummaryComponent implements OnInit {
+  private bookingService = inject(BookingService);
   private router = inject(Router);
+  getBooking$!: Observable<Booking>;
   @Input() step: number = 1;
+
+  ngOnInit(): void {
+    this.getBooking$ = this.bookingService.getBookingData();
+  }
 
   continue() {
     if (this.step == 1) this.router.navigate(['booking/choose-profissional']);
