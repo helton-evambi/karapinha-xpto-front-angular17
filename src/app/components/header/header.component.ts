b719fd68ef43e0e342515118a5248f74594dad18
Helton Evambi <heltonevambi@gmail.com>
2024-06-26 16:53:07 +0100

    inser icon icon in header menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
 import { RouterLink } from '@angular/router';
+import { ButtonComponent } from '../button/button.component';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { matLoginOutline } from '@ng-icons/material-icons/outline';
+import { ionPersonCircleSharp } from '@ng-icons/ionicons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [LogoComponent, RouterLink],
+  imports: [LogoComponent, ButtonComponent, RouterLink, NgIconComponent],
+  viewProviders: [
+    provideIcons({
+      matLoginOutline,
+      ionPersonCircleSharp,
+    }),
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
