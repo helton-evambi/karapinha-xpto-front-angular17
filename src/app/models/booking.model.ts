195212ed9c42707a868695aa00b5da06ad544f59
Helton Evambi <heltonevambi@gmail.com>
2024-06-28 20:54:21 +0100

    add user propriety to booking model

--- src/app/models/booking.model.ts
+++ src/app/models/booking.model.ts
@@ -1,9 +1,12 @@
 import { BookingServices } from './booking-service.model';
 import { Time } from './time.model';
+import { User } from './user.model';
 
 export interface Booking {
   BookingId?: number;
   Services: BookingServices[];
+  User?: User;
+  UserId: number;
   Price: number;
   Status: string;
 }
