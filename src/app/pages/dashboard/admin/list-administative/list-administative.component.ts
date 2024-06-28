f27a29c0540c1c5b7e58fe2d85ff9ecaa05ce361
Helton Evambi <heltonevambi@gmail.com>
2024-06-28 20:59:12 +0100

    consume user service

--- src/app/pages/dashboard/admin/list-administative/list-administative.component.ts
+++ src/app/pages/dashboard/admin/list-administative/list-administative.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { DashboardContainerComponent } from '../../dashboard-container/dashboard-container.component';
 import { MenuItensComponent } from '../../../../components/dashboard/menu-itens/menu-itens.component';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { ionBarChart, ionPerson, ionPeople } from '@ng-icons/ionicons';
+import {
+  ionBarChart,
+  ionPerson,
+  ionPeople,
+  ionPersonCircleSharp,
+  ionPersonCircleOutline,
+} from '@ng-icons/ionicons';
 import { TableComponent } from '../../../../components/dashboard/table/table.component';
 import { RouterLink } from '@angular/router';
 import { ModalComponent } from '../../../../components/modal/modal.component';
 import { ActionCardComponent } from '../../../../components/dashboard/action-card/action-card.component';
+import { UserService } from '../../../../services/user.service';
+import { User } from '../../../../models/user.model';
+import { Observable } from 'rxjs';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-list-administative',
@@ -19,20 +29,32 @@ import { ActionCardComponent } from '../../../../components/dashboard/action-car
     ModalComponent,
     ActionCardComponent,
     RouterLink,
+    AsyncPipe,
+    NgIf,
   ],
   viewProviders: [
     provideIcons({
       ionBarChart,
       ionPerson,
       ionPeople,
+      ionPersonCircleOutline,
     }),
   ],
   templateUrl: './list-administative.component.html',
   styleUrl: './list-administative.component.scss',
 })
-export class ListAdministativeComponent {
+export class ListAdministativeComponent implements OnInit {
+  private userService = inject(UserService);
+
+  administatives$!: Observable<User[]>;
+
   administrativeId: number = 0;
   modalVisibility: boolean = false;
+
+  ngOnInit(): void {
+    this.administatives$ = this.userService.getUsers();
+  }
+
   openModal(administrativeId: number) {
     this.modalVisibility = true;
     this.administrativeId = administrativeId;
