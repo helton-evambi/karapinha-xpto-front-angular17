5ff37f06d2f255fe278bc4665554cea80ac3c830
Helton Evambi <heltonevambi@gmail.com>
2024-07-06 16:29:49 +0100

    change servicos page

--- src/app/pages/servicos/servicos.component.ts
+++ src/app/pages/servicos/servicos.component.ts
@@ -1,13 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { ServicesContentComponent } from '../../components/services-content/services-content.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { ServiceService } from '../../services/service.service';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Service } from '../../models/service.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-servicos',
   standalone: true,
-  imports: [HeaderComponent, ServicesContentComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    ServicesContentComponent,
+    FooterComponent,
+    AsyncPipe,
+    NgIf,
+    RouterLink,
+  ],
   templateUrl: './servicos.component.html',
   styleUrl: './servicos.component.scss',
 })
-export class ServicosComponent {}
+export class ServicosComponent implements OnInit {
+  private serviceService = inject(ServiceService);
+  services$!: Observable<Service[]>;
+  ngOnInit(): void {
+    this.services$ = this.serviceService.getServices();
+  }
+}
