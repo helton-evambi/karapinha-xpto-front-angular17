0f094a9e227d8e1d802d9338803e40dcbc5269d1
Helton Evambi <heltonevambi@gmail.com>
2024-07-06 16:27:59 +0100

    create register category page

---
+++ src/app/pages/dashboard/administrative/register-category/register-category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-category',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-category.component.html',
+  styleUrl: './register-category.component.scss'
+})
+export class RegisterCategoryComponent {
+
+}
