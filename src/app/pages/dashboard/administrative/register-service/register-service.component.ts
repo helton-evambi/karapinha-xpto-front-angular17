7ede769308d1fe5fe544d7cb521575ba159c2e42
Helton Evambi <heltonevambi@gmail.com>
2024-07-06 16:27:24 +0100

    create register service page

---
+++ src/app/pages/dashboard/administrative/register-service/register-service.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-service',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-service.component.html',
+  styleUrl: './register-service.component.scss'
+})
+export class RegisterServiceComponent {
+
+}
