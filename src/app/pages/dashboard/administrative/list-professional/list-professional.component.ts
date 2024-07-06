ce5e8df82466a4437bb9147c3fa128f1d53ef830
Helton Evambi <heltonevambi@gmail.com>
2024-07-06 16:57:26 +0100

    create page list-professional

---
+++ src/app/pages/dashboard/administrative/list-professional/list-professional.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-professional',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-professional.component.html',
+  styleUrl: './list-professional.component.scss'
+})
+export class ListProfessionalComponent {
+
+}
