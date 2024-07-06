142a6179da7a0be90181783c56257c0dbc503ad2
Helton Evambi <heltonevambi@gmail.com>
2024-07-06 16:55:35 +0100

    list service page

---
+++ src/app/pages/dashboard/administrative/list-service/list-service.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-service',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-service.component.html',
+  styleUrl: './list-service.component.scss'
+})
+export class ListServiceComponent {
+
+}
