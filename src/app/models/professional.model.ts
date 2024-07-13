df861de98c7927d9cefee44e9ce93ee9bd40a27a
Helton Evambi <heltonevambi@gmail.com>
2024-07-13 08:04:50 +0100

    update proprieties in professional

--- src/app/models/professional.model.ts
+++ src/app/models/professional.model.ts
@@ -2,13 +2,13 @@ import { Category } from './category.model';
 import { Time } from './time.model';
 
 export interface Professional {
-  ProfissionalId: number;
+  ProfessionalId: number;
   FirstName: string;
   LastName: string;
   EmailAddress: string;
   PhotoUrl: string;
   IdCard: string;
-  PhomeNumber: number;
+  PhoneNumber: number;
   Category: Category;
   Times: Time[];
 }
