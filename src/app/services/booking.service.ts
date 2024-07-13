caba05d08e24af8b52aa3babae92e3c11a37449f
Helton Evambi <heltonevambi@gmail.com>
2024-07-13 07:58:43 +0100

    create init booking method to update booking

--- src/app/services/booking.service.ts
+++ src/app/services/booking.service.ts
@@ -25,14 +25,31 @@ export class BookingService {
     return this.bookingDataSubject$.asObservable();
   }
 
+  initBooking(booking: Booking) {
+    this.bookingData = booking;
+    this.bookingData.UserId =
+      this.bookingData.User?.UserId ?? Number(sessionStorage.getItem('id'));
+    this.bookingDataSubject$.next(this.bookingData);
+  }
   addService(service: Service) {
-    this.bookingData.Services.push({
-      Service: service,
-      Professional: undefined,
-      Date: undefined,
-      Time: undefined,
-    });
-    this.bookingData.Price += service.Price;
+    const serviceIndex = this.bookingData.Services.findIndex(
+      (s) => s.Service.ServiceId === service.ServiceId
+    );
+    if (serviceIndex !== -1) {
+      this.bookingData.Services[serviceIndex].Service.Price;
+      this.bookingData.Services.splice(serviceIndex, 1);
+      this.bookingData.Price -= service.Price;
+    } else {
+      // Se o serviço não existir, adicioná-lo e ajustar o preço
+      this.bookingData.Services.push({
+        Service: service,
+        Professional: undefined,
+        Date: undefined,
+        Time: undefined,
+      });
+      this.bookingData.Price += service.Price;
+    }
+    // Atualiza o observable com os dados atualizados
     this.bookingDataSubject$.next(this.bookingData);
   }
 
@@ -102,8 +119,26 @@ export class BookingService {
     return this.http.post('https://localhost:44350/api/bookings', booking);
   }
 
-  deleteCategory(id: number): Observable<any> {
+  deleteBooking(id: number): Observable<any> {
     return this.http.delete<any>(`https://localhost:44350/api/bookings/${id}`);
+  }
+
+  updateBooking(id: number, bookingData: Booking): Observable<any> {
+    return this.http.put<any>(
+      `https://localhost:44350/api/bookings/${id}`,
+      bookingData
+    );
+  }
+
+  updateBookingStatus(id: number, status: string): Observable<any> {
+    const body = `"${status}"`;
+    return this.http.put<any>(
+      `https://localhost:44350/api/bookings/updateStatus/${id}`,
+      body,
+      {
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
   }
 
   getRevenueForToday(): Observable<number> {
