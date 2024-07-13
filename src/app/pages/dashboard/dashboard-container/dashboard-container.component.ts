d4ba44ac1e6070ae7351b6d045364def2b2331c8
Helton Evambi <heltonevambi@gmail.com>
2024-07-13 08:00:22 +0100

    implement logout in dashboard

--- src/app/pages/dashboard/dashboard-container/dashboard-container.component.ts
+++ src/app/pages/dashboard/dashboard-container/dashboard-container.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { ionSettingsSharp, ionLogOutOutline } from '@ng-icons/ionicons';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard-container',
   standalone: true,
-  imports: [NgIconComponent],
+  imports: [NgIconComponent, RouterLink],
   viewProviders: [
     provideIcons({
       ionSettingsSharp,
@@ -16,7 +18,12 @@ import { ionSettingsSharp, ionLogOutOutline } from '@ng-icons/ionicons';
   styleUrl: './dashboard-container.component.scss',
 })
 export class DashboardContainerComponent {
+  private authService = inject(AuthService);
   @Input() uesrName: string = '';
   @Input() userRole: String = '';
   @Input() pageTitle: string = '';
+
+  logout() {
+    this.authService.logout();
+  }
 }
