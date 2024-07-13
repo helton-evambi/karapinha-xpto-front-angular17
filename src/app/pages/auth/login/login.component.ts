c3fba32a0da1dd9a4c340523dff1a0afdf9bdc4d
Helton Evambi <heltonevambi@gmail.com>
2024-07-13 08:05:53 +0100

    implement redirecting in login

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, Inject, OnInit, inject } from '@angular/core';
 import { PrimaryInputComponent } from '../../../components/primary-input/primary-input.component';
 import { ButtonComponent } from '../../../components/button/button.component';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { matKey, matAlternateEmail } from '@ng-icons/material-icons/baseline';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import {
   FormControl,
   FormGroup,
@@ -39,6 +39,8 @@ interface LoginForm {
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup<LoginForm>;
+  private route = inject(Router);
+
   private toastr = inject(ToastrService);
   private authService = inject(AuthService);
   ngOnInit(): void {
@@ -54,12 +56,24 @@ export class LoginComponent implements OnInit {
         .login(this.loginForm.value.identifier, this.loginForm.value.password)
         .subscribe({
           next: (value) => {
-            this.toastr.success(
-              'Login efetuado com sucesso ' + value.FirstName + value.LastName
-            );
+            sessionStorage.setItem('isLogged', 'true');
             sessionStorage.setItem('id', String(value.UserId));
             sessionStorage.setItem('username', value.Username);
             sessionStorage.setItem('role', value.Role);
+            sessionStorage.setItem('status', value.Status);
+            if (value.Status === 'pending') {
+              if (value.Role === 'administrative') {
+                this.toastr.warning('Precisa editar os seu dados de perfíl');
+                this.route.navigate(['/administrative/edit-profile']);
+              } else this.toastr.warning('A sua conta necessita de Ativação');
+            } else {
+              if (value.Role === 'user') this.route.navigate(['/']);
+
+              if (value.Role === 'admin') this.route.navigate(['/dashboard']);
+
+              if (value.Role === 'administrative')
+                this.route.navigate(['/administrative/dashboard']);
+            }
           },
           error: () =>
             this.toastr.error('Identificaçao ou Palavra passe inválida'),
