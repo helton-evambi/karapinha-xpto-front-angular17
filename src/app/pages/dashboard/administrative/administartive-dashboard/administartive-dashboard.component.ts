df115633b6c337bd9a509cf54e17ce2c4f73cbda
Helton Evambi <heltonevambi@gmail.com>
2024-07-14 09:51:16 +0100

    get dashboard datas

--- src/app/pages/dashboard/administrative/administartive-dashboard/administartive-dashboard.component.ts
+++ src/app/pages/dashboard/administrative/administartive-dashboard/administartive-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import {
@@ -18,6 +18,13 @@ import { MenuItensComponent } from '../../../../components/dashboard/menu-itens/
 import { TableComponent } from '../../../../components/dashboard/table/table.component';
 import { DashboardContainerComponent } from '../../dashboard-container/dashboard-container.component';
 import { matBookmarks } from '@ng-icons/material-icons/baseline';
+import { Observable } from 'rxjs';
+import { Professional } from '../../../../models/professional.model';
+import { Service } from '../../../../models/service.model';
+import { BookingService } from '../../../../services/booking.service';
+import { ProfessionalService } from '../../../../services/professional.service';
+import { ServiceService } from '../../../../services/service.service';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-administartive-dashboard',
@@ -29,6 +36,8 @@ import { matBookmarks } from '@ng-icons/material-icons/baseline';
     DashboardCardComponent,
     TableComponent,
     RouterLink,
+    AsyncPipe,
+    CommonModule,
   ],
   viewProviders: [
     provideIcons({
@@ -48,4 +57,56 @@ import { matBookmarks } from '@ng-icons/material-icons/baseline';
   templateUrl: './administartive-dashboard.component.html',
   styleUrl: './administartive-dashboard.component.scss',
 })
-export class AdministartiveDashboardComponent {}
+export class AdministartiveDashboardComponent {
+  private bookingService = inject(BookingService);
+  private professionalService = inject(ProfessionalService);
+  private serviceService = inject(ServiceService);
+
+  revenue!: number;
+  topProfessionals$!: Observable<Professional[]>;
+  mostRequestedService$!: Observable<Service>;
+  leastRequestedService$!: Observable<Service>;
+
+  revenueOptions: boolean = false;
+  ngOnInit(): void {
+    this.bookingService
+      .getRevenueForToday()
+      .subscribe((value) => (this.revenue = value));
+    this.topProfessionals$ = this.professionalService.getTop5Professionals();
+    this.mostRequestedService$ = this.serviceService.getMostRequestedService();
+    this.leastRequestedService$ =
+      this.serviceService.getLeastRequestedService();
+  }
+
+  showRevenueOptions() {
+    this.revenueOptions = true;
+  }
+
+  getRevenueForToday() {
+    this.bookingService
+      .getRevenueForToday()
+      .subscribe((value) => (this.revenue = value));
+    this.revenueOptions = false;
+  }
+
+  getRevenueForYesterday() {
+    this.bookingService
+      .getRevenueForYesterday()
+      .subscribe((value) => (this.revenue = value));
+    this.revenueOptions = false;
+  }
+
+  getRevenueForCurrentMonth() {
+    this.bookingService
+      .getRevenueForCurrentMonth()
+      .subscribe((value) => (this.revenue = value));
+    this.revenueOptions = false;
+  }
+
+  getRevenueForLastMonth() {
+    this.bookingService
+      .getRevenueForLastMonth()
+      .subscribe((value) => (this.revenue = value));
+    this.revenueOptions = false;
+  }
+}
